5031a4c27658a8a76695120fdd3973f0376569b6
Isaac Johnson <isaac.johnson@gmail.com>
2023-09-21 18:56:47 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+variable "projectid" {
+  type        = string
+  description = "The name of the GCP project"
+}
+
+variable "bucketname" {
+  type        = string
+  description = "The name of the bucket to try and create"
+}
