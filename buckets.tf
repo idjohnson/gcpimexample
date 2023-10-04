f1f980c9447af977079b756a4be4dc79982e86c1
Isaac Johnson <isaac.johnson@gmail.com>
2023-10-03 19:20:41 -0500

    remove kms

--- buckets.tf
+++ buckets.tf
@@ -23,21 +23,6 @@ resource "google_storage_bucket" "auto-expire" {
   }
 }
 
-resource "google_kms_key_ring" "terraform_state" {
-  name     = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  location = "us"
-}
-
-resource "google_kms_crypto_key" "terraform_state_bucket" {
-  name            = "test-terraform-state-bucket"
-  key_ring        = google_kms_key_ring.terraform_state.id
-  rotation_period = "86400s"
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
@@ -49,8 +34,5 @@ resource "google_storage_bucket" "default" {
   storage_class = "STANDARD"
   versioning {
     enabled = true
-  }
-  encryption {
-    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
   }
 }
